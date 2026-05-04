861b3641306fcef0c29b6746cf8a2331af95972f
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-05-04 20:26:53 +0300

    update: refined api-config.js functionality

--- src/lib/api-config.js
+++ src/lib/api-config.js
@@ -1,1 +1,1 @@
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost/pos-system/api';
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE ||'http://localhost/pos-system/api';
